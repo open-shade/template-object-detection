39b0c67633b7772cc95e7074c058e8759ebf102a
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 16:04:52 -0400

    update image classification template

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,16 +1,19 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="microsoft"
-declare -a MODEL_VERSIONS=( "<model versions here" )
+MODEL_NAME="<name>"
+declare -a MODEL_VERSIONS=( "<model versions here>" )
 
 for VERSION in "${ROS_VERSIONS[@]}"
 do
   for MODEL_VERSION in "${MODEL_VERSIONS[@]}"
   do
     ROS_VERSION="$VERSION"
-    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
+    HF_VERSION="$MODEL_NAME-$MODEL_VERSION"
+    TAG="$MODEL_VERSION"
+    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_TAG="$TAG",_MODEL_VERSION="$HF_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
-    echo Dispached "$MODEL_VERSION" on ROS "$ROS_VERSION"
+    echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
 done
 
